ca9f5348eddc9e628324ab69ce2493ef8a59a55f
Yurii <gapirov21@gmail.com>
2023-11-04 23:12:35 +0200

    Step1 feedback+statistics

---
+++ src/components/App/App.jsx
@@ -0,0 +1,37 @@
+import { GlobalStyle } from '../GlobalStyle';
+import { Component } from 'react';
+import { Container } from './App.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  //викликаємо публічну фу-цію і отримуєму type відгуку
+
+  updateFeedback = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1>Please leave feedback</h1>
+        <button onClick={() => this.updateFeedback('good')}>Good</button>
+        <button onClick={() => this.updateFeedback('neutral')}>Neutral</button>
+        <button onClick={() => this.updateFeedback('bad')}>Bad</button>
+        <div>
+          <h2>Statistics</h2>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+        </div>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border: 2px solid grey;
+  border-radius: 8px;
+  margin: 36px;
+  padding: 36px;
+  max-width: 400px;
+`;
